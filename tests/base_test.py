95e01d6c4a87495b0d243cea0f2a7bbe19a80d5b
d.poloka <d.poloka@levi9.com>
2016-01-25 12:07:28 +0200

    delete .idea files and modify base_test class

--- tests/base_test.py
+++ tests/base_test.py
@@ -2,16 +2,18 @@ import pytest
 from selenium import webdriver
 import core.config
 
-
 @pytest.fixture(scope='class')
 def setup(request):
     core.config.driver = webdriver.Firefox()
+    core.config.driver.implicitly_wait(10)
+    core.config.driver.verificationErrors = []
+    core.config.driver.accept_next_alert = True
+    core.config.driver.maximize_window()
 
     def teardown():
         core.config.driver.quit()
 
     request.addfinalizer(teardown)
-
 
 @pytest.mark.usefixtures("setup")
 class BaseTest(object):
