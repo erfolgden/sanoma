31af5a4090ace45041004ce5c706c3c32374248a
d.poloka <d.poloka@levi9.com>
2016-02-10 14:52:04 +0200

    Add desire_cap parameters: browser, browser_version,os, os_version, resolution

--- core/config.py
+++ core/config.py
@@ -8,3 +8,44 @@ LIBELLE_URL = "http://www.libelle.nl/"
 
 # delay for wait until function
 UI_MAX_RESPONSE_TIME = 25.0
+
+# Browserstack credentials:
+# USERNAME = 'xxxx', BROWSERSTACK_KEY = 'xxxxx'
+USERNAME = 'den377'
+BROWSERSTACK_KEY = '6XpuCRXaQtKHWNQfSzz4'
+
+# Browserstack parameters:
+
+# BROWSER = 'IE', 'Edge', 'Firefox', 'Chrome'
+BROWSER = 'Edge'
+
+# 'IE': '11.0'
+# 'Edge': '12.0'
+# 'Firefox': '44.0, '43.0', '42.0', '41.0', '40.0', '39.0', '38.0', '37.0', '36.0', '35.0', '34.0', '33.0', '32.0'
+# 'Chrome': '48.0, '47.0', '46.0', '45.0', '44.0', '43.0', '42.0', '41.0', '40.0', '39.0', '38.0', '37.0'
+BROWSER_VERSION = '12.0'
+
+# OS = 'Windows', 'OS X'
+OS = 'Windows'
+
+# 'Windows' = '10', '8.1', '8', '7', 'XP'
+# 'OS X' = 'El Capitan', 'Yosemite', 'Mavericks', 'Mountain Lion'
+OS_VERSION = '8'
+
+# '1024x768', '1280x960', '1280x1024', '1600x1200', '1920x1080'
+RESOLUTION = '1024x768'
+
+DESIRED_CAP = {'browser': BROWSER,
+               'browser_version': BROWSER_VERSION,
+               'os': OS,
+               'os_version': OS_VERSION,
+               'resolution': RESOLUTION
+               }
+
+DESIRED_CAP_MOBILE = {'browserName': 'android',
+                      'platform': 'ANDROID',
+                      'device': 'Samsung Galaxy S5'}
+
+# URL
+#BROWSERSTACK_URL = "http://%s:%s@hub.browserstack.com:80/wd/hub" % (USERNAME, BROWSERSTACK_KEY)
+
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 
 from selenium import webdriver
+from core.config import DESIRED_CAP, USERNAME, BROWSERSTACK_KEY
+desired_cap = {'browser': 'IE', 'browser_version': '11.0', 'os': 'Windows', 'os_version': '8.1', 'resolution': '1024x768'}
+url= 'http://den378:7Lh6qWZ5xQrYg8iveqmo@hub.browserstack.com:80/wd/hub'
 
 browsers = {"ff": 'firefox', 'ch': 'chrome'}
 
@@ -23,10 +26,18 @@ class DriverManager(object):
 
         if type == 'firefox':
             self._instance = webdriver.Firefox()
-        else:
+        if type == 'chrome':
             # fixme: path to browser is hardcoded now. Need to instatiate browser somewhere in discussed place and get him from ENV
             self._instance = webdriver.Chrome("D:\\Install\\chromedriver_win32\\chromedriver.exe")
         self._instance.maximize_window()
+        if type == 'bs':
+            # implement logic to connect with browserstack
+            if not (USERNAME and BROWSERSTACK_KEY):
+                raise Exception("Please provide your BrowserStack username and access key")
+            else:
+                self._instance = webdriver.Remote(command_executor='http://den378:7Lh6qWZ5xQrYg8iveqmo@hub.browserstack.com:80/wd/hub',desired_capabilities=desired_cap)
+        # self._instance = webdriver.Chrome("path//to//webdriver")
+
         return self._instance
 
     @property
@@ -37,6 +48,7 @@ class DriverManager(object):
 
     def stop(self):
         self._instance.close()
+        self._instance.quit()
 
 
 def pytest_addoption(parser):
