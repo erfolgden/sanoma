2d8488bbae6d2f38cf86a8924475828a81ce71be
erfolgden <denis.poloka@gmail.com>
2016-02-21 12:49:34 +0200

    Update testLibelleHomePage.py

--- tests/libelle/testLibelleHomePage.py
+++ tests/libelle/testLibelleHomePage.py
@@ -23,18 +23,17 @@ All element should be present on the page
 """
 class TestLoginFunctionality(BaseTest):
 
-
-    def test_HomePage(self, driver):
-        """
-        Check Home page url's and title's
-        :param driver:instance of selenium driver
-        :return:
-        """
-        pages = Pages(driver)
-        homePage = pages.navigateTo().select_libelle_home_page()
-        open_home_page = homePage.open_libelle()
-        assert open_home_page == LIBELLE_URL, 'Incorrect url-address'
-        assert homePage.check_libelle_title() == homePage.PAGE_TITLE, 'Incorrect url-address'
+#    def test_HomePage(self, driver):
+#        """
+#        Check Home page url's and title's
+#        :param driver:instance of selenium driver
+#        :return:
+#        """
+#        pages = Pages(driver)
+#        homePage = pages.navigateTo().select_libelle_home_page()
+#        open_home_page = homePage.open_libelle()
+#        assert open_home_page == LIBELLE_URL, 'Incorrect url-address'
+#        assert homePage.check_libelle_title() == homePage.PAGE_TITLE, 'Incorrect url-address'
 
     def test_LoginPage(self, driver):
         """
