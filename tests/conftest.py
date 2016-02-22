b033b20752559c17c309a261d1f06d0d75e32760
erfolgden <denis.poloka@gmail.com>
2016-02-22 18:40:05 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 from selenium import webdriver
 from core.config import DESIRED_CAP, USERNAME, BROWSERSTACK_KEY
-desired_cap = {'browser': 'Firefox', 'browser_version': '43.0', 'os': 'Windows', 'os_version': '8.1', 'resolution': '1920x1200', 'browserstack.debug':'true', 'browserstack.local': 'true'}
+desired_cap = {'browser': 'Firefox', 'browser_version': '43.0', 'os': 'Windows', 'os_version': '8.1', 'resolution': '1920x1200', 'browserstack.debug':'true'}
 url= 'http://den378:7Lh6qWZ5xQrYg8iveqmo@hub.browserstack.com:80/wd/hub'
 
 browsers = {"ff": 'firefox', 'ch': 'chrome', 'bs': 'browserstack'}
