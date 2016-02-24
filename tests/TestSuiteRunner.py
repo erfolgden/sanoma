b93b89fadca9c660b9a8d6e35f4abe967344ff0a
k.tyshchenko <kostya.tyshchenko@gmail.com>
2016-02-24 11:20:10 +0200

    add html report option

--- tests/TestSuiteRunner.py
+++ tests/TestSuiteRunner.py
@@ -2,7 +2,12 @@
 import argparse
 import os
 import sys
+import webbrowser
 
+
+def get_directory_files(path):
+    for _, _, files in os.walk(path):
+        return [file for file in files]
 
 def main(argv):
     """
@@ -17,18 +22,28 @@ def main(argv):
     parser.add_argument("-browser", "--browser", default="",
                         help="specify browser type: Chrome = 'ch', Firefox = 'ff', "
                              "browserstack = 'bs'")
+    parser.add_argument("-html", "--html", default="",
+                        help="specify html report must be with expansion .html"
+                             "(e.g -html=report.html)")
 
     args = parser.parse_args()
     browser = args.browser
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "")
     if browser:
         browser = "--browser=" + args.browser
     if args.test_list:
-        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "") + args.test_list
-        for directories, sub_dirs, files in os.walk(path):
-                for file in files:
-                    if file.startswith("test") and not file.endswith(".pyc"):
+        test_path = path + args.test_list
+        test_list = get_directory_files(test_path)
+        for i in test_list:
+            if i.startswith("test") and not i.endswith(".pyc"):
                         os.system("py.test {} {}".format(
-                            browser, os.path.join(path, "") + file))
+                            browser, os.path.join(test_path, "") + i))
+    if args.html:
+        html_path = get_directory_files(path)
+        for i in html_path:
+            if i.endswith(".html"):
+                webbrowser.open_new_tab(i)
+
 
 if __name__ == "__main__":
     main(sys.argv)
